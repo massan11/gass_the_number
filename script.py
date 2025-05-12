675eea361d199505ebb85cf0d1e909e2eca24919
massan11 <mhassan.zoho@gmail.com>
2025-05-12 13:29:38 +0330

    implement main game loop and guessing logic for "Guess the Number"

--- script.py
+++ script.py
@@ -1,9 +1,24 @@
 import random
 rand_num = random.randint(0,10)
 
-input_num = int(input("Pick a number between 0 and 10: "))
-
-if rand_num == input_num:
-    print("Well done!")
-else:
-    print(f"You made a mistake, the number was {rand_num}. ")
+try_guess = 0
+while try_guess < 3:
+    input_num = int(input("Pick a number between 0 and 10: "))
+    if rand_num == input_num:
+        print("Well done!")
+        break
+    elif input_num < rand_num:
+        try_guess += 1
+        if try_guess < 3:
+            print("Guess a bigger number.")
+        else:
+            print("Game over.")
+        
+    elif input_num > rand_num:
+        try_guess += 1
+        if try_guess < 3:
+            print("Guess a lower number.")
+        else:
+            print("Game over.")
+    else:
+        print("Erorr!")
