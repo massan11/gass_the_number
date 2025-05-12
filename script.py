5ca21593ef43c2aa531733aed6dfe6ad4f86aef8
massan11 <mhassan.zoho@gmail.com>
2025-05-12 13:59:30 +0330

    refactor game logic to improve readability and maintainability

--- script.py
+++ script.py
@@ -1,24 +1,25 @@
 import random
-rand_num = random.randint(0,10)
 
+rand_num = random.randint(0,10)
 try_guess = 0
-while try_guess < 3:
-    input_num = int(input("Pick a number between 0 and 10: "))
+max_attempts = 3
+
+while try_guess < max_attempts:
+    try:
+        input_num = int(input("Pick a number between 0 and 10: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
+    
+    try_guess += 1
+
     if rand_num == input_num:
         print("Well done!")
         break
     elif input_num < rand_num:
-        try_guess += 1
-        if try_guess < 3:
-            print("Guess a bigger number.")
-        else:
-            print("Game over.")
-        
-    elif input_num > rand_num:
-        try_guess += 1
-        if try_guess < 3:
-            print("Guess a lower number.")
-        else:
-            print("Game over.")
+        print("Guess a bigger number.")
     else:
-        print("Erorr!")
+        print("Guess a lower number.")
+        
+    if try_guess == max_attempts:
+        print(f"Game over. The number was {rand_num}.")
